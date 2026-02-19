86035d1906aa7e3132b18e39eb527740a549257a
anomixer <anomixer@users.noreply.github.com>
2026-02-19 16:13:26 +0000

    deploy: e2852ff9e4f2156ec30d5343a576487b4800d940

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/runner/work/clemens_iigs/clemens_iigs/host
+# Build directory: /home/runner/work/clemens_iigs/clemens_iigs/build-em/host
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("harness")
+subdirs("ext/imgui")
---
+++ harness/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/runner/work/clemens_iigs/clemens_iigs/host/harness
+# Build directory: /home/runner/work/clemens_iigs/clemens_iigs/build-em/host/harness
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
